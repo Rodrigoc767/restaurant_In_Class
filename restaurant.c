0cd205a6a5cabf770b3a356f44ee411b1038fb82
Rodrigo Cibrian <cibrian@cpp.edu>
2021-09-30 21:41:29 -0700

    Initial Outline

---
+++ restaurant.c
@@ -0,0 +1,20 @@
+/* 
+    Write a program that computes the tax and tip on a restaurant bill for a patron. 
+
+    1. Ask the user for the tax percent of tax and the gratuity percent they would like to give and assign it to the appropriate
+        variable.
+
+    2. Create a random number generator from 0-3 that will randomly select the meal cost.
+
+    0 Salad: $9.95
+    1 Soup: $4.55
+    2 Sandwich: $13.25
+    3 Pizza: $22.35
+
+    3. I will assign the randomly generated number to an int.
+
+    4. I will use an if else statement to determine the mathmatical equation and return and 
+        print the total tip, tax, meal cost, and grand total
+
+
+*/
